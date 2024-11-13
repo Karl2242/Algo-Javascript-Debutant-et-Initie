95ac0487af1b43678d5d122775cfe873506f776e
Karl2242 <kchikhicloud@gmail.com>
2024-11-13 16:25:23 +0100

    exo 7

--- script.js
+++ script.js
@@ -21,17 +21,57 @@ console.log("Bienvenue dans le monde de la programation");
 
 // let result = 0;
 
-
-
 // for(let i = 0; i < maPhrase.length; i = i + 1){
 //    for(let voyelles = 0; voyelles < mesVoyelles.length; voyelles = voyelles + 1){
 
 // if(maPhrase[i] == mesVoyelles[voyelles]){
 // result = result + 1
-// console.log(result) 
+// console.log(result)
 // }
 //    }
 //    }
 
 //    console.log("mon nombre de voyelle est de " + result)
+
+// const maPhrase = "CECI EST MA PHRASE";
+// const alphabet = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+// let result = 0;
+
+// for(let i = 0; i < alphabet.length; i = i + 1){
+
+// for(let voy = 0; voy < maPhrase.length; voy = voy + 1){
+
+//     if(maPhrase[voy] == alphabet[i]){
+
+//         result = result + 1
+
+//     }
+
+// }
+// if(result > 0){
+//  console.log(alphabet[i])
+// console.log(result)
+// }
+
+// result = 0;
+// }
+
+// let mesNombres = [122, -12, 16, 60, -32, 60, -152];
+
+// let positiv = [];
+
+// let negatif = [];
+
+// for (let i = 0; i < mesNombres.length; i = i + 1) {
+//   if (mesNombres[i] >= 0) {
+//     positiv.push(mesNombres[i]);
+//   } else {
+//     negatif.push(mesNombres[i]);
+//   }
+// }
+
+
+// console.log("le tableau est " + positiv);
+// console.log("le tableau est " + negatif);
+
 

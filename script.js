b150c458d291ec9a921495af4f78d3febced1105
Karl2242 <kchikhicloud@gmail.com>
2024-11-13 11:15:38 +0100

    5 exo

--- script.js
+++ script.js
@@ -1,18 +1,37 @@
-console.log("Hello world")
-console.log("Bienvenue dans le monde de la programation")
+console.log("Hello world");
+console.log("Bienvenue dans le monde de la programation");
 
-let nombre1 = 5;
-let nombre2 = 10;
+// let nombre1 = 5;
+// let nombre2 = 10;
 
-console.log(nombre1 + nombre2)
+// console.log(nombre1 + nombre2)
 
-let fruit = ["pomme", "cerise", "banane"]
+// let fruit = ["pomme", "cerise", "banane"]
 
-fruit.forEach((element) => {
-   console.log(element) 
-})
+// fruit.forEach((element) => {
+//    console.log(element)
+// })
 
-for(let i = 0; i <= 20; i += 2){
-    console.log(i)
-}
+// for(let i = 0; i <= 20; i += 2){
+//     console.log(i)
+// }
+
+// let maPhrase = "Voici plein de voyelle bonne change igo";
+// const mesVoyelles = ["a", "e", "i", "o", "u"];
+
+// let result = 0;
+
+
+
+// for(let i = 0; i < maPhrase.length; i = i + 1){
+//    for(let voyelles = 0; voyelles < mesVoyelles.length; voyelles = voyelles + 1){
+
+// if(maPhrase[i] == mesVoyelles[voyelles]){
+// result = result + 1
+// console.log(result) 
+// }
+//    }
+//    }
+
+//    console.log("mon nombre de voyelle est de " + result)
 
